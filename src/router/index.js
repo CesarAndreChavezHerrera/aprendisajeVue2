941b2b27809cdda487c4835d1f17a152c5456e5d
Cesar Chavez <65314179+CesarAndreChavezHerrera@users.noreply.github.com>
2022-02-19 20:28:12 -0600

    se creo el conversor de longitud

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Longitud from "../views/longitud.vue" //inicial mayuscula 
+
 
 Vue.use(VueRouter)
 
@@ -11,15 +13,11 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
+    path: '/longitud',
+    name: 'longitud',
+    component: Longitud // debe ser letra mayuscula la inicial para que no se confunda esta tontera
+  },
+  
 ]
 
 const router = new VueRouter({
---
+++ src/views/longitud.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="longitud">
+        
+        <Longitud/>
+    </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+
+import Longitud from '../components/longitud.vue'
+
+
+export default {
+  name: 'longitud',
+  components: {
+    
+    Longitud //hacemos llamado a la importacion
+    
+  }
+}
+</script>
